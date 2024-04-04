8cb8e7b57e003a23bbc7d9566638e5f0ddcf402a
Snehil Shubham <snehil1693@gmail.com>
2024-04-04 13:30:18 +0530

    Updating the new comments in 03-vars

--- 03-vars.sh
+++ 03-vars.sh
@@ -5,3 +5,9 @@ echo A = $A
 
 No_of_users=$(who | wc -l)
 echo Number of users = $No_of_users
+
+DATE = "2024-04-04"
+echo welcome, today date is $DATE
+
+DATE = $(date +%F)
+echo Welcome, Today date is $DATE
