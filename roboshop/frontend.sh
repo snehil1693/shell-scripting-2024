ccf41b44769b1f788e321e7bff134b5229574f71
Snehil Shubham <snehil1693@gmail.com>
2024-04-04 23:57:30 +0530

    Making changes in frontend13

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -3,33 +3,24 @@
 LOG=/tmp/roboshop.log
 rm -f $LOG
 
+STAT_CHECK(){
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32m done\e[0m"
+  else
+    echo -e "\e[31m fail\e[0m"
+  exit 1
+  fi
+}
+
 echo -n -e "Installing Nginx\t\t..."
 
 yum install nginx -y  &>>$LOG
-echo $?
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m done\e[0m"
-else
-  echo -e "\e[31m fail\e[0m"
-  exit 1
-fi
+STAT_CHECK $?
 
 echo -n -e "Enabling Nginx\t\t\t..."
 systemctl enable nginx  &>>$LOG
-echo $?
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m done\e[0m"
-else
-  echo -e "\e[31m fail\e[0m"
-  exit 1
-fi
+STAT_CHECK $?
 
 echo -n -e "Starting Nginx\t\t\t..."
 systemctl start nginx  &>>$LOG
-echo $?
-if [ $? -eq 0 ];then
-  echo -e "\e[32m done\e[0m"
-else
-  echo -e "\e[31m fail\e[0m"
-  exit 1
-fi
+STAT_CHECK $?
