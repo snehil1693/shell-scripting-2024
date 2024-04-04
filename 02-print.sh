d415965b299a4bdb24e9984a1fd71007270cd718
Snehil Shubham <snehil1693@gmail.com>
2024-04-04 10:19:51 +0530

    Updating the new comments in 02-print

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/bash
 
 echo "Hello World"
+
+echo -e "Hello World,\n\tWelcome"
+
+echo -e "\e[31mMessage in Red \e[0m"
+
+echo -e "\e[32mMessage in Yellow \e[0m"
+
+echo -e "Message in plane text"
