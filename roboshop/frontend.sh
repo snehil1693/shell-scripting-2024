908afec80c357d47689642128e5ce9606a19b6ee
Snehil Shubham <snehil1693@gmail.com>
2024-04-05 00:33:18 +0530

    Making changes in frontend

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -3,8 +3,24 @@
 source common.sh
 
 PRINT "Installing Nginx"
-
 yum install nginx -y  &>>$LOG
+STAT_CHECK $?
+
+PRINT "Download Frontend\t"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
+STAT_CHECK $?
+
+PRINT "Remove Old Htdocs\t"
+cd /usr/share/nginx/html &>>$LOG && rm -rf * &>>$LOG
+STAT_CHECK $?
+
+PRINT "Extract Frontend Archive"
+unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* . &>>$LOG && mv static/* . &>>$LOG && rm -rf frontend-master static
+&>>$LOG
+STAT_CHECK $?
+
+PRINT "Update Roboshop Config\t"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
 STAT_CHECK $?
 
 PRINT "Enabling Nginx\t"
