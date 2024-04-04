67965cbe56b69d40c19d027a63d111f54db1e6eb
Snehil Shubham <snehil1693@gmail.com>
2024-04-04 19:25:33 +0530

    Making changes in frontend5

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
 
 LOG=/tmp/roboshop.log
+rm -f $LOG
 
 echo -e "Installing Nginx\t\t...\t\e[32mdone\e[0m"
 
-yum install nginx -y  >>$LOG
+yum install nginx -y  &>>$LOG
 
 echo "Enabling Nginx"
-systemctl enable nginx  >>$LOG
+systemctl enable nginx  &>>$LOG
 
 echo "Starting Nginx"
-systemctl start nginx  >>$LOG
+systemctl start nginx  &>>$LOG
