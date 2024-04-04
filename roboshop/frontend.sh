a16321ca6172d140faaebe3ef2f082300b831d2d
Snehil Shubham <snehil1693@gmail.com>
2024-04-05 00:07:44 +0530

    Making a new file common.sh and keeping all the codes there as they will be used in every components

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/bash
 
-LOG=/tmp/roboshop.log
-rm -f $LOG
-
-STAT_CHECK(){
-  if [ $1 -eq 0 ]; then
-    echo -e "\e[32m done\e[0m"
-  else
-    echo -e "\e[31m fail\e[0m"
-  exit 1
-  fi
-}
-
-PRINT() {
-  echo -n -e "$1\t\t..."
-}
+source commin.sh
 
 PRINT "Installing Nginx"
 
