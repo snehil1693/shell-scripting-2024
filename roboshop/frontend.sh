17c79ccfed8279ab1261c00cafefa29a29ccf868
Snehil Shubham <snehil1693@gmail.com>
2024-04-04 20:14:45 +0530

    Making changes in frontend6

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -6,9 +6,12 @@ rm -f $LOG
 echo -e "Installing Nginx\t\t...\t\e[32mdone\e[0m"
 
 yum install nginx -y  &>>$LOG
+echo $?
 
 echo "Enabling Nginx"
 systemctl enable nginx  &>>$LOG
+echo $?
 
 echo "Starting Nginx"
 systemctl start nginx  &>>$LOG
+echo $?
