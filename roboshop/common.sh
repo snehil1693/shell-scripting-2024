6aaedbc803b159e98c413cc80f5dbec9f38f1602
Snehil Shubham <snehil1693@gmail.com>
2024-04-05 00:15:10 +0530

    Making changes in commmon01

--- roboshop/common.sh
+++ roboshop/common.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
 
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]; then
+  echo -e "\e[31mYou should be root user / sudo user to run this script\e[0m"
+  exit 2
+fi
+
 LOG=/tmp/roboshop.log
 rm -f $LOG
 
