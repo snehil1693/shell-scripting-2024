256b31a74ae8891deb6d5c48196be6ae989bed5f
Snehil Shubham <snehil1693@gmail.com>
2024-04-05 17:03:09 +0530

    Making changes in common

--- roboshop/common.sh
+++ roboshop/common.sh
@@ -14,10 +14,12 @@ STAT_CHECK(){
     echo -e "\e[32m done\e[0m"
   else
     echo -e "\e[31m fail\e[0m"
+    echo -e "\e[33m Check the log file for more details, Log File - $LOG\e[0m"
   exit 1
   fi
 }
 
 PRINT() {
+  echo -e "\n########################################\t$1\t###########################" &>>$LOG
   echo -n -e "$1\t\t..."
 }
